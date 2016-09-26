a942bca96f979d28bd12a9d4b2a096cf75ea2803
Sai Num Town <sainumtown@gmail.com>
2016-09-27 02:19:45 +0630

    presenter finished persistence

--- app/src/main/java/com/padc/recipes/data/vos/RecipeVO.java
+++ app/src/main/java/com/padc/recipes/data/vos/RecipeVO.java
@@ -3,6 +3,8 @@ package com.padc.recipes.data.vos;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.gson.annotations.SerializedName;
@@ -109,6 +111,18 @@ public class RecipeVO {
         this.photos = photos;
     }
 
+    public void setCategory(CategoryVO category) {
+        this.category = category;
+    }
+
+    public void setPresenter(PresenterVO presenter) {
+        this.presenter = presenter;
+    }
+
+    public void setTags(List<TagVO> tags) {
+        this.tags = tags;
+    }
+
     public static void saveRecipes(List<RecipeVO> recipeList) {
         Context context = RecipesApp.getContext();
         ContentValues[] recipeCVs = new ContentValues[recipeList.size()];
@@ -121,6 +135,13 @@ public class RecipeVO {
 
             // insert into category
             RecipeVO.saveCategory(recipe.category);
+
+            // insert into presenter
+            if(recipe.getPresenter().getPresenter_id() != null &&
+                    !TextUtils.isEmpty(recipe.getPresenter().getPresenter_id())){
+                RecipeVO.savePresenter(recipe.presenter);
+            }
+
         }
 
         //Bulk insert into attractions.
@@ -129,12 +150,30 @@ public class RecipeVO {
         Log.d(RecipesApp.TAG, "Bulk inserted into recipe table : " + insertedCount);
     }
 
+    private static void savePresenter(PresenterVO presenter) {
+        ContentValues presenterCV = new ContentValues();
+        presenterCV.put(RecipeContract.PresenterEntry.COLUMN_PRESENTER_ID, presenter.getPresenter_id());
+        presenterCV.put(RecipeContract.PresenterEntry.COLUMN_PRESENTER_NAME, presenter.getPresenter_name());
+
+        Context context = RecipesApp.getContext();
+        Uri insertedUri = context.getContentResolver().insert(RecipeContract.PresenterEntry.CONTENT_URI, presenterCV);
+
+        Log.d(RecipesApp.TAG, "Presenter Inserted Uri : " + insertedUri);
+
+    }
+
     private static void saveCategory(CategoryVO category) {
-        ContentValues cv =new ContentValues();
-        cv.put(RecipeContract.CategoryEntry.COLUMN_CATEGORY_ID,category.getCategory_id());
-        cv.put(RecipeContract.CategoryEntry.COLUMN_CATEGORY_NAME,category.getCategory_name());
-        cv.put(RecipeContract.CategoryEntry.COLUMN_DESCRIPTION,category.getDescription());
-        cv.put(RecipeContract.CategoryEntry.COLUMN_IMAGE,category.getImage());
+        ContentValues categoryCV = new ContentValues();
+        categoryCV.put(RecipeContract.CategoryEntry.COLUMN_CATEGORY_ID, category.getCategory_id());
+        categoryCV.put(RecipeContract.CategoryEntry.COLUMN_CATEGORY_NAME, category.getCategory_name());
+        categoryCV.put(RecipeContract.CategoryEntry.COLUMN_DESCRIPTION, category.getDescription());
+        categoryCV.put(RecipeContract.CategoryEntry.COLUMN_IMAGE, category.getImage());
+
+        Context context = RecipesApp.getContext();
+        Uri insertedUri = context.getContentResolver().insert(RecipeContract.CategoryEntry.CONTENT_URI, categoryCV);
+
+        Log.d(RecipesApp.TAG, "Category Inserted Uri : " + insertedUri);
+
     }
 
     private static void saveRecipeImages(String title, String[] images) {
@@ -161,6 +200,7 @@ public class RecipeVO {
         cv.put(RecipeContract.RecipeEntry.COLUMN_NOTE, note);
         cv.put(RecipeContract.RecipeEntry.COLUMN_VIDEO, video);
         cv.put(RecipeContract.RecipeEntry.COLUMN_CATEGORY_ID, category.getCategory_id());
+        cv.put(RecipeContract.RecipeEntry.COLUMN_PRESENTER_ID, presenter.getPresenter_id());
 
         return cv;
     }
@@ -176,6 +216,11 @@ public class RecipeVO {
         CategoryVO category = new CategoryVO();
         category.setCategory_id(Integer.parseInt(category_id));
         recipe.category = category;
+
+        String presenter_id = data.getString(data.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_PRESENTER_ID));
+        PresenterVO presenter = new PresenterVO();
+        presenter.setPresenter_id(presenter_id);
+        recipe.presenter = presenter;
 
         return recipe;
     }
@@ -197,5 +242,48 @@ public class RecipeVO {
         String[] imageArray = new String[images.size()];
         images.toArray(imageArray);
         return imageArray;
+    }
+
+    public static CategoryVO loadCategoryByCategoryId(String categoryId) {
+
+        Context context = RecipesApp.getContext();
+        CategoryVO category = new CategoryVO();
+
+        Cursor cursor = context.getContentResolver().query(RecipeContract.CategoryEntry.buildCategoryUriWithId(categoryId),
+                null, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+
+                category.setCategory_id(Integer.parseInt(cursor.getString(cursor.getColumnIndex(RecipeContract.CategoryEntry.COLUMN_CATEGORY_ID))));
+                category.setCategory_name(cursor.getString(cursor.getColumnIndex(RecipeContract.CategoryEntry.COLUMN_CATEGORY_NAME)));
+                category.setDescription(cursor.getString(cursor.getColumnIndex(RecipeContract.CategoryEntry.COLUMN_DESCRIPTION)));
+                category.setImage(cursor.getString(cursor.getColumnIndex(RecipeContract.CategoryEntry.COLUMN_IMAGE)));
+            } while (cursor.moveToNext());
+        }
+
+
+        return category;
+    }
+
+    public static PresenterVO loadPresenterByPresenterId(String presenterId) {
+
+        Context context = RecipesApp.getContext();
+        PresenterVO presenter = new PresenterVO();
+
+        Cursor cursor = context.getContentResolver().query(RecipeContract.PresenterEntry.buildPresenteriWithId(presenterId),
+                null, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+
+                presenter.setPresenter_id(String.valueOf(Integer.parseInt(cursor.getString(cursor.getColumnIndex(RecipeContract.PresenterEntry.COLUMN_PRESENTER_ID)))));
+                presenter.setPresenter_name(cursor.getString(cursor.getColumnIndex(RecipeContract.PresenterEntry.COLUMN_PRESENTER_NAME)));
+
+            } while (cursor.moveToNext());
+        }
+
+
+        return presenter;
     }
 }
