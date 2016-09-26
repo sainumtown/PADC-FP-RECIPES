6ae6577a8dcd32d874050b2f474c523fe88873b1
Sai Num Town <sainumtown@gmail.com>
2016-09-26 21:11:38 +0630

    Recipe loader pattern Recipe persistence started

--- app/src/main/java/com/padc/recipes/data/persistence/RecipeProvider.java
+++ app/src/main/java/com/padc/recipes/data/persistence/RecipeProvider.java
@@ -16,37 +16,167 @@ import android.text.TextUtils;
  */
 public class RecipeProvider extends ContentProvider {
 
+    public static final int RECIPE = 100;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private RecipeDBHelper mRecipeDBHelper;
+
+    private static final String sRecipetitleSelection = RecipeContract.RecipeEntry.COLUMN_TITLE + " = ?";
 
     @Override
     public boolean onCreate() {
-        return false;
+        mRecipeDBHelper = new RecipeDBHelper(getContext());
+        return true;
     }
 
     @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        Cursor queryCursor;
+        int matchUri = sUriMatcher.match(uri);
+
+        switch (matchUri) {
+            case RECIPE:
+                String recipeTitle = RecipeContract.RecipeEntry.getTitleFromParam(uri);
+                if (!TextUtils.isEmpty(recipeTitle)) {
+                    selection = sRecipetitleSelection;
+                    selectionArgs = new String[]{recipeTitle};
+                }
+                queryCursor = mRecipeDBHelper.getReadableDatabase().query(RecipeContract.RecipeEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null, //group_by
+                        null, //having
+                        sortOrder);
+                break;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri : " + uri);
+        }
+        Context context = getContext();
+
+        if (context != null) {
+            queryCursor.setNotificationUri(context.getContentResolver(), uri);
+        }
+        return queryCursor;
     }
 
     @Nullable
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int matchUri = sUriMatcher.match(uri);
+        switch (matchUri) {
+            case RECIPE:
+                return RecipeContract.RecipeEntry.DIR_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri : " + uri);
+        }
     }
 
     @Nullable
     @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final SQLiteDatabase db = mRecipeDBHelper.getWritableDatabase();
+        final int matchUri = sUriMatcher.match(uri);
+        Uri insertedUri;
+
+        switch (matchUri) {
+            case RECIPE: {
+                long _id = db.insert(RecipeContract.RecipeEntry.TABLE_NAME, null, contentValues);
+                if (_id > 0) {
+                    insertedUri = RecipeContract.RecipeEntry.buildRecipeUri(_id);
+                } else {
+                    throw new SQLException("Failed to insert row into " + uri);
+                }
+                break;
+
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri : " + uri);
+        }
+
+        Context context = getContext();
+        if (context != null) {
+            context.getContentResolver().notifyChange(uri, null);
+        }
+
+        return insertedUri;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = mRecipeDBHelper.getWritableDatabase();
+        String tableName = getTableName(uri);
+        int insertedCount = 0;
+
+        try {
+            db.beginTransaction();
+            for (ContentValues cv : values) {
+                long _id = db.insert(tableName, null, cv);
+                if (_id > 0) {
+                    insertedCount++;
+                }
+            }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+            db.close();
+        }
+
+        Context context = getContext();
+        if (context != null) {
+            context.getContentResolver().notifyChange(uri, null);
+        }
+
+        return insertedCount;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        final SQLiteDatabase db = mRecipeDBHelper.getWritableDatabase();
+        int rowDeleted;
+        String tableName = getTableName(uri);
+
+        rowDeleted = db.delete(tableName, selection, selectionArgs);
+        Context context = getContext();
+        if (context != null && rowDeleted > 0) {
+            context.getContentResolver().notifyChange(uri, null);
+        }
+        return rowDeleted;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mRecipeDBHelper.getWritableDatabase();
+        int rowUpdated;
+        String tableName = getTableName(uri);
+
+        rowUpdated = db.update(tableName, contentValues, selection, selectionArgs);
+        Context context = getContext();
+        if (context != null && rowUpdated > 0) {
+            context.getContentResolver().notifyChange(uri, null);
+        }
+        return rowUpdated;
+    }
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(RecipeContract.CONTENT_AUTHORITY, RecipeContract.PATH_RECIPES, RECIPE);
+
+        return uriMatcher;
+    }
+
+    private String getTableName(Uri uri) {
+        final int matchUri = sUriMatcher.match(uri);
+
+        switch (matchUri) {
+            case RECIPE:
+                return RecipeContract.RecipeEntry.TABLE_NAME;
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri : " + uri);
+        }
     }
 }
