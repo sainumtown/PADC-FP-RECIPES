1cd2b6db04a5410d93e26f40a4b3e9c32643821c
Sai Num Town <sainumtown@gmail.com>
2016-10-01 17:50:59 +0630

    Resolve for load township

--- app/src/main/java/com/padc/recipes/data/models/RestaurantModel.java
+++ app/src/main/java/com/padc/recipes/data/models/RestaurantModel.java
@@ -45,6 +45,9 @@ public class RestaurantModel extends BaseModel {
         //keep the data in persistent layer.
         RestaurantVO.saveRestaurants(mRestaurantList);
 
+        // save township
+        
+
         //broadcastRestaurantLoadedWithEventBus();
     }
 
